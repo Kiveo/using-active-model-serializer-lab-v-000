a40f4d996e3d6a210a7c99b4553f4c8b860ee128
Maverick Tesini <mavericktesini@gmail.com>
2018-08-22 07:49:09 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :description, :inventory
+  attributes :id, :name, :description, :price, :inventory
   # has_many :orders, through: :ordered_products
   belongs_to :order
 end
